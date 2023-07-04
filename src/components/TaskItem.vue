13adeaad29f947d474c1bb935bd96317ad683641
viktoor48 <71779943+viktoor48@users.noreply.github.com>
2023-07-04 16:13:29 +0300

    fix TaskItem

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -3,55 +3,46 @@
     <div class="task-item__header">
       <div class="left-filed">
         <input
-          @click="handleCheck(task)"
-          :checked="task.checked"
+          @click="handleCheck(props.task)"
+          :checked="props.task.checked"
           type="checkbox"
           class="task-item__checkbox mr-2"
         />
         <span
-          :class="{ strikethrough: task.checked }"
+          :class="{ strikethrough: props.task.checked }"
           class="task-name font-medium"
           >{{ task.name }}</span
         >
       </div>
-      <button @click="deleteTask(index)">Удалить</button>
+      <button @click="deleteTask(props.index)">Удалить</button>
     </div>
     <div class="task-item__footer">
-      <div class="task-item__date">{{ task.date }}</div>
+      <div class="task-item__date">{{ props.task.date }}</div>
     </div>
   </li>
 </template>
 
-<script>
-export default {
-  name: "TaskItem",
-  props: {
-    task: {
-      type: Object,
-      required: true,
-    },
-    index: {
-      type: Number,
-      required: true,
-    },
-  },
-  setup(props, { emit }) {
-    const handleCheck = (task) => {
-      emit("handleCheck", task);
-    };
+<script setup>
+import { defineProps, defineEmits } from "vue";
 
-    const deleteTask = (ind) => {
-      emit("deleteTask", ind);
-    };
+const emit = defineEmits(["handleCheck", "deleteTask"]);
+const props = defineProps({
+  task: {
+    type: Object,
+    required: true,
+  },
+  index: {
+    type: Number,
+    required: true,
+  },
+});
 
-    return {
-      handleCheck,
-      deleteTask,
-    };
-  },
-  mounted() {
-    console.log(this.task);
-  },
+const handleCheck = (task) => {
+  emit("handleCheck", task);
+};
+
+const deleteTask = (ind) => {
+  emit("deleteTask", ind);
 };
 </script>
 
